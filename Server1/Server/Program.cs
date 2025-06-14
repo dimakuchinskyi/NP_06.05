6c7ac4aea8845b262f588ce8ecf79abe288e3f03
Кучинський Дмитро Максимович <131793372+dimakuchinskyi@users.noreply.github.com>
2025-06-14 12:28:25 +0300

    Add files via upload

---
+++ Server1/Server/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        var listener = new TcpListener(IPAddress.Any, 5000);
+        listener.Start();
+        Console.WriteLine("Server started...");
+
+        while (true)
+        {
+            using var client = await listener.AcceptTcpClientAsync();
+            _ = HandleClientAsync(client);
+        }
+    }
+
+    static async Task HandleClientAsync(TcpClient client)
+    {
+        var stream = client.GetStream();
+        var buffer = new byte[1024];
+        int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+        string received = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+        Console.WriteLine($"Received: {received}");
+
+        string response = "Hello client";
+        byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+        await stream.WriteAsync(responseBytes, 0, responseBytes.Length);
+    }
+}
