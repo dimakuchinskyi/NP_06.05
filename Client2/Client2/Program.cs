c2ca6481d5033461f720b6835391830d74087d9b
Кучинський Дмитро Максимович <131793372+dimakuchinskyi@users.noreply.github.com>
2025-06-14 12:35:25 +0300

    Add files via upload

---
+++ Client2/Client2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("Enter 'date' or 'time':");
+string command = Console.ReadLine();
+
+using var client = new TcpClient();
+await client.ConnectAsync("127.0.0.1", 5000);
+using var stream = client.GetStream();
+var data = Encoding.UTF8.GetBytes(command);
+await stream.WriteAsync(data, 0, data.Length);
+
+var buffer = new byte[256];
+int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+Console.WriteLine("Server response: " + response);
---
+++ Server2/Server2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var listener = new TcpListener(IPAddress.Any, 5000);
+listener.Start();
+Console.WriteLine("Async server started...");
+
+while (true)
+{
+    var client = await listener.AcceptTcpClientAsync();
+    _ = Task.Run(async () =>
+    {
+        using (client)
+        using (var stream = client.GetStream())
+        {
+            var buffer = new byte[256];
+            int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+            var request = Encoding.UTF8.GetString(buffer, 0, bytesRead).Trim();
+
+            string response = request switch
+            {
+                "date" => DateTime.Now.ToShortDateString(),
+                "time" => DateTime.Now.ToLongTimeString(),
+                _ => "Unknown command"
+            };
+
+            var responseBytes = Encoding.UTF8.GetBytes(response);
+            await stream.WriteAsync(responseBytes, 0, responseBytes.Length);
+        }
+    });
+}
